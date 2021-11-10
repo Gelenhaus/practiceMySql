09403257104b8f56ad6112c7f1963566ccf9f324
Gerrit Elenbaas <84500222+Gelenhaus@users.noreply.github.com>
2021-11-10 16:26:36 -0700

    got a single database with one table working

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Ronald', 'Firbank', 1, 2),
+  ('Virginia', 'Woolf', 1, 3),
+  ('Piers', 'Gaveston', 0, 2),
+  ('Charles', 'LeRoi', 1, 1),
+  ('Katherine', 'Mansfield', 1, 3),
+  ('Dora', 'Carrington', 0, 2),
+  ('Edward', 'Bellamy', 0, 3),
+  ('Montague', 'Summers', 1, 3),
+  ('Octavia', 'Butler', 1, 2),
+  ('Unica', 'Zurn', 1, 3);
